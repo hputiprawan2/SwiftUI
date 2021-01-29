00ccf7576a46614292cdb6412079e070fff87a36
Hanna Putiprawan <hputiprawan2@gmail.com>
2021-01-28 16:20:29 -0800

    [Add] Dicee-SwiftUI app

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Hanna Putiprawan on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
