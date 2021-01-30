64d9438a31ff1a3484c20019e5d59b290e10c893
Hanna Putiprawan <hputiprawan2@gmail.com>
2021-01-30 00:55:16 -0800

    [Add] I Am Rich project

---
+++ I Am Rich SwiftUI/I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Hanna Putiprawan on 1/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Image("diamond").resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
